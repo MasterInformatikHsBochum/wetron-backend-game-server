b38d24721342c9625d8ddbe2d7db2f0ae414f435
sebo <sebastian.246grueter@gmail.com>
2017-09-27 14:18:26 +0200

    readonly widht and height of grid

--- src/grid.ts
+++ src/grid.ts
@@ -1,24 +1,12 @@
 export class Grid {
-    private _width: number;
-    private _height: number;
     private _grid;
 
-    constructor (width: number, height: number) {
-        this._width = width;
-        this._height = height;
-        this._grid = new Array(this._width);
+    constructor (readonly width: number, readonly height: number) {
+        this._grid = new Array(width);
 
         for (let i = 0; i < this._grid.length; i++) {
-            this._grid[i] = new Array(this._height).fill(0);
+            this._grid[i] = new Array(height).fill(0);
         }
-    }
-
-    get width(): number {
-        return this._width;
-    }
-
-    get height(): number {
-        return this._height;
     }
 
     public isCoordinateEmpty(x: number, y: number): boolean {
