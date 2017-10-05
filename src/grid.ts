6fdca0f98ae92946eadff45a1f91b7f94a3c533b
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2017-10-05 08:32:39 +0200

    Updated class Grid

--- src/grid.ts
+++ src/grid.ts
@@ -12,11 +12,11 @@ export class Grid {
     public isCoordinateEmpty(x: number, y: number): boolean {
         if (x < 0) {
             return false;
-        } else if (x > this.width) {
+        } else if (x > this.width - 1) {
             return false;
         } else if (y < 0) {
             return false;
-        } else if (y > this.height) {
+        } else if (y > this.height - 1) {
             return false;
         } else if (this._grid[x][y] > 0) {
             return false;
